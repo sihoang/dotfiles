14b49912e1267a5d0a2fd28a2452424a846b101d
Hoang Nguyen <sihoang7@gmail.com>
2018-06-04 02:46:13 -0700

    update sync to perform real actions

--- sync.sh
+++ sync.sh
@@ -1,41 +1,13 @@
 #!/bin/sh
 # This script checks if the local configs are in-sync with dotfiles repo
+# Perform diff localversion gitversion
 # Usage: ./sync.sh
-# Flags:
-#   -g git to local i.e. git takes precedence CAUTION! BACK UP YOUR LOCAL FILES!!!
-#   -l local to git i.e. local takes precedence
 
-gflag="";
-lflag="";
 
-check_already_specified() {
-  if [ ! -z $2 ]; then
-    echo ERROR: $1 already specified. Choose either -g or -l.;
-    exit 1;
-  fi
-}
-
-# Parse flags
-while getopts "gl" flag; do
-  case "$flag" in
-    g)
-      check_already_specified "-l" $lflag;
-      gflag="true";
-      ;;
-    l)
-      check_already_specified "-g" $gflag;
-      lflag="true";
-      ;;
-    *)
-      exit 1;
-      ;;
-  esac
-done
-
-# Declare files to be tracked
+# Declare items (files and/or directories) to be tracked
 # Array of tuples: local,git
 # NO SPACES IN BETWEEN
-declare -a TRACKED_FILES=(
+declare -a TRACKED_ITEMS=(
  "$HOME/.config/nvim/init.vim","init.vim"
  "$HOME/.tmux.conf",".tmux.conf"
  "$HOME/.zshrc",".zshrc"
@@ -49,12 +21,52 @@ declare -a TRACKED_FILES=(
 # TODO sync with remote git
 # git pull
 
-for pair in "${TRACKED_FILES[@]}"; do
+total_items=${#TRACKED_ITEMS[@]};
+current_items=1;
+for pair in "${TRACKED_ITEMS[@]}"; do
   IFS=",";
   set $pair;
-  diff -ruN --unidirectional-new-file $1 $2 | colordiff;
 
-  # TODO: Consolidate based on gflag/lflag
+  echo "*********************************************************************
+    Checking item $current_items of $total_items
+  ";
 
+  diff_content=$(diff -ruN --unidirectional-new-file $1 $2 | colordiff);
+
+  if [ "$diff_content" == "" ]; then
+    echo "Up to date $1 $2";
+  else
+    # clear;
+    echo $diff_content;
+    echo "
+      What do you want to do with this item?
+
+      0) Do nothing. (Default)
+
+      1) Take the local's version.
+          cp $1 $2
+
+      2) Take the git's version and keep a local backup.
+          mv $1 $1.backup
+          cp $2 $1
+
+    ";
+
+    read -p " Select an action (0, 1 or 2): " action;
+    case $action in
+      1)
+        echo "cp $1 $2";
+        cp $1 $2;
+        ;;
+      2)
+        echo "cp $2 $1";
+        mv $1 $1.backup;
+        cp $2 $1;
+        ;;
+    esac
+  fi
+
+  echo ;
+  current_items=$((current_items + 1))
   unset IFS;
 done
