89494b50d509dd53a132b4a48aee19281304c2ca
Hoang Nguyen <hoang@wetrust.io>
2019-12-08 18:24:09 -0800

    wg0.sh use local vars for readability

--- wg0.sh
+++ wg0.sh
@@ -4,22 +4,25 @@
 # without having to use wg-quick
 # Make sure there is
 # 1. /etc/wireguard/wg0.conf
-# 2. Edit the address and dns server accordingly
+# 2. Edit the wg network address and dns server accordingly
 
 set -ex
 
 [[ $UID != 0 ]] && exec sudo -E "$(readlink -f "$0")" "$@"
 
+WG_LOCAL_IP=10.0.0.2
+DNS_NS=1.1.1.1
+
 up() {
     ip link add dev wg0 type wireguard
-    ip address add dev wg0 Address
+    ip address add dev wg0 $WG_LOCAL_IP
     wg setconf wg0 /etc/wireguard/wg0.conf
     wg set wg0 fwmark 1234
     ip link set wg0 up
     ip route add default dev wg0 table 2468
     ip rule add not fwmark 1234 table 2468
     ip rule add table main suppress_prefixlength 0
-    echo "nameserver 1.1.1.1" | resolvconf -a tun.wg0 -x
+    echo "nameserver $DNS_NS" | resolvconf -a tun.wg0 -x
 }
 
 down() {
