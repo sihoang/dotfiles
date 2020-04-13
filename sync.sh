b73c76aa34ee8b020a41babee25b4f78a9d9a9a3
Hoang Nguyen <hoang@wetrust.io>
2020-04-12 20:20:07 -0700

    use diff instead of colordiff

--- sync.sh
+++ sync.sh
@@ -22,24 +22,24 @@ declare -a TRACKED_ITEMS=(
 # TODO sync with remote git
 # git pull
 
-total_items=${#TRACKED_ITEMS[@]};
-current_items=1;
+total_items=${#TRACKED_ITEMS[@]}
+current_items=1
 for pair in "${TRACKED_ITEMS[@]}"; do
-  IFS=",";
-  set $pair;
+  IFS=","
+  set $pair
 
   echo "*********************************************************************
     Checking item $current_items of $total_items
   ";
 
-  diff_content=$(colordiff -ruN --unidirectional-new-file $1 $2);
-  diff_error=$?;
+  diff_content=$(diff -ruN --unidirectional-new-file $1 $2)
+  diff_error=$?
 
   if [ $diff_error -eq 0 ]; then
-    echo "Up to date $1 $2";
+    echo "Up to date $1 $2"
   elif [ $diff_error -eq 1 ]; then
-    # clear;
-    echo $diff_content;
+    # clear
+    echo "$diff_content"
     echo "
       What do you want to do with this item?
 
@@ -52,18 +52,18 @@ for pair in "${TRACKED_ITEMS[@]}"; do
           mv $1 $1.backup
           cp $2 $1
 
-    ";
+    "
 
-    read -p " Select an action (0, 1 or 2): " action;
+    read -p " Select an action (0, 1 or 2): " action
     case $action in
       1)
-        echo "cp $1 $2";
-        cp $1 $2;
+        echo "cp $1 $2"
+        cp $1 $2
         ;;
       2)
-        echo "cp $2 $1";
-        mv $1 $1.backup;
-        cp $2 $1;
+        echo "cp $2 $1"
+        mv $1 $1.backup
+        cp $2 $1
         ;;
     esac
   else
@@ -75,7 +75,7 @@ for pair in "${TRACKED_ITEMS[@]}"; do
     "
   fi
 
-  echo ;
+  echo 
   current_items=$((current_items + 1))
-  unset IFS;
+  unset IFS
 done
